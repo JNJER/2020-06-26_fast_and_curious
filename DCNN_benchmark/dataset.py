eeece4f835c24680c158a6cf3158b2434d45329a
JNJER <jeremie.jnicolas@gmail.com>
2020-09-07 00:23:09 +0200

    Ajout de résultats HP-Pavillon + mise à jour dataset.py & init.py

--- DCNN_benchmark/dataset.py
+++ DCNN_benchmark/dataset.py
@@ -1,19 +1,20 @@
 
 from DCNN_benchmark.init import *
 
-list_dir = os.listdir(path)
-
+# check if the folder exist
 if os.path.isdir(path):
-    print("The folder " , folder, " already exists, it includes: ", list_dir)   # check if the folder exist and create one 
+    list_dir = os.listdir(path)
+    print("The folder " , folder, " already exists, it includes: ", list_dir)
     
+# no folder, creating one 
 else :
     print(f"No existing path match for this folder, creating a folder at {path}")
     os.makedirs(path)
 
-print(list_dir)
-if len(list_dir) < N_labels : # if there aren't anough labels download some more
-    print('This folder do not have anough classes, downloading some more') # using the downloader
-    cmd =f"python3 ImageNet-datasets-downloader/downloader.py -data_root {root} -data_folder {folder} -images_per_class {N_images_per_class} -use_class_list True  -class_list {id_dl} -multiprocessing_workers 0"
+# if the folder is empty, download the images using the ImageNet-Datasets-Downloader
+if len(list_dir) < N_labels : 
+    print('This folder do not have anough classes, downloading some more') 
+    cmd =f"python3 ImageNet-Datasets-Downloader/downloader.py -data_root {root} -data_folder {folder} -images_per_class {N_images_per_class} -use_class_list True  -class_list {id_dl} -multiprocessing_workers 0"
     print('Command to run : ', cmd)
     os.system(cmd) # running it
     list_dir = os.listdir(path)
@@ -23,14 +24,14 @@ elif len(os.listdir(path)) == N_labels :
           
 else : # if there are to many folders delete some
     print('The folder have to many classes, deleting some')
-    while len(os.listdir(path)) > N_labels : 
-        for elem in os.listdir(path):
-            contenu = os.listdir(f'{path}/{elem}')
+    for elem in os.listdir(path):
+        contenu = os.listdir(f'{path}/{elem}')
+        if len(os.listdir(path)) > N_labels :
             for x in contenu:
                 os.remove(f'{path}/{elem}/{x}') # delete exces folders
-        try:
-            os.rmdir(f'{path}/{elem}')
-        except:
-            os.remove(f'{path}/{elem}')
+            try:
+                os.rmdir(f'{path}/{elem}')
+            except:
+                os.remove(f'{path}/{elem}')
     list_dir = os.listdir(path)
-    print("Now the folder " , folder, f" contains :", os.listdir(path)) 
+    print("Now the folder " , folder, f" contains :", os.listdir(path))
--- DCNN_benchmark/init.py
+++ DCNN_benchmark/init.py
@@ -1,16 +1,16 @@
+
 # import libs
 import os
 import time 
 from time import strftime,gmtime
 import json
 import time 
-import os
 import numpy as np
 import imageio
 from numpy import random
 from torchvision.datasets import ImageFolder
 import matplotlib.pyplot as plt
-import urllib.request
+
 # to store results
 import pandas as pd
 
@@ -30,7 +30,7 @@ datetag = '2020-08-27'
 image_size = 256 # default image resolution
 image_sizes = 2**np.arange(6, 10) # resolutions explored
 
-N_images_per_class = 100
+N_images_per_class = 10
 #i_labels = random.randint(1000, size=(N_labels)) # Random choice
 i_labels = [409, 530, 892, 487, 920, 704, 879, 963, 646, 620 ] # Pre-selected classes
 N_labels = len(i_labels)
@@ -40,12 +40,12 @@ root = 'data'
 folder = 'imagenet_classes_100'
 path = os.path.join(root, folder) # data path
 
-with open('ImageNet-datasets-downloader/imagenet_classes.txt') as f:
+with open('ImageNet-Datasets-Downloader/imagenet_classes.txt') as f:
     labels = [line.strip() for line in f.readlines()]
 labels[0].split(', ')
 labels = [label.split(', ')[1].lower().replace('_', ' ') for label in labels]
 
-class_loader = 'ImageNet-datasets-downloader/imagenet_class_info.json'
+class_loader = 'ImageNet-Datasets-Downloader/imagenet_class_info.json'
 with open(class_loader, 'r') as fp: # get all the classes on the data_downloader
     name = json.load(fp)
 
